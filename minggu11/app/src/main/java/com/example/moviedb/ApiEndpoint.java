dcb9d231e01bedc3ced13ad7236ad5117ccec6b8
sitirohilah <99700683+sitirohilah@users.noreply.github.com>
2023-05-14 18:56:54 +0700

    Add files via upload

---
+++ minggu11/app/src/main/java/com/example/moviedb/ApiEndpoint.java
@@ -0,0 +1,14 @@
+package com.example.moviedb;
+
+public class ApiEndpoint {
+
+    public static String BASEURL = "https://api.themoviedb.org/3/";
+    public static String APIKEY = "api_key=ebbb1dad084a6e41630b394e4a7cc4c3";
+    public static String LANGUAGE = "&language=en-US";
+    public static String SEARCH_MOVIE = "search/movie?";
+    public static String QUERY = "&query=";
+    public static String MOVIE_PLAYNOW = "movie/now_playing?";
+    public static String MOVIE_POPULAR = "discover/movie?";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w780/";
+
+}
